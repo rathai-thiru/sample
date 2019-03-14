01947d647e01e7d88a293975dc1fc72436e8bec6
Rathai Thiru <rathai@Rathais-MacBook-Pro.local>
2019-03-14 21:31:15 +1300

    revert back

--- _ss_environment.php
+++ _ss_environment.php
@@ -7,10 +7,11 @@ $user = (getenv('OPENSHIFT_MYSQL_DB_USERNAME')) ? getenv('OPENSHIFT_MYSQL_DB_USE
 $pass = (getenv('OPENSHIFT_MYSQL_DB_PASSWORD')) ? getenv('OPENSHIFT_MYSQL_DB_PASSWORD') : putenv('OPENSHIFT_MYSQL_DB_PASSWORD=root') ;
 
 define('SS_DATABASE_CLASS', 'MySQLPDODatabase');
-define('SS_DATABASE_SERVER', 'mysql');
-define('SS_DATABASE_PORT', '3306');
-define("SS_DATABASE_USERNAME", 'password');
-define('SS_DATABASE_NAME', 'database');
+define('SS_DATABASE_SERVER', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('SS_DATABASE_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define("SS_DATABASE_USERNAME", getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('SS_DATABASE_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('SS_DATABASE_NAME', getenv('OPENSHIFT_APP_NAME'));
 
 define('SS_DEFAULT_ADMIN_USERNAME', 'admin');
 define('SS_DEFAULT_ADMIN_PASSWORD', 'admin');
@@ -18,3 +19,4 @@ define('SS_DEFAULT_ADMIN_PASSWORD', 'admin');
 global $_FILE_TO_URL_MAPPING;
 $openshift_app_url = (getenv('OPENSHIFT_APP_DNS')) ? getenv('OPENSHIFT_APP_DNS') : putenv('OPENSHIFT_APP_DNS=openshift_app_dns') ;
 $_FILE_TO_URL_MAPPING[dirname(__FILE__)] = 'http://' . $openshift_app_url . '/';
+
