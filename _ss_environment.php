5592409510e1d61d72fb85a00cae91277272d1da
rathai-thiru <39539276+rathai-thiru@users.noreply.github.com>
2019-03-18 16:44:17 +1300

    Update _ss_environment.php

--- _ss_environment.php
+++ _ss_environment.php
@@ -1,17 +1,17 @@
 <?php
 define('SS_ENVIRONMENT_TYPE', 'dev');
 
-$host = (getenv('OPENSHIFT_MYSQL_DB_HOST')) ? getenv('OPENSHIFT_MYSQL_DB_HOST') : putenv('OPENSHIFT_MYSQL_DB_HOST=localhost') ;
-$port = (getenv('OPENSHIFT_MYSQL_DB_PORT')) ? getenv('OPENSHIFT_MYSQL_DB_PORT') : putenv('OPENSHIFT_MYSQL_DB_PORT=3306');
-$user = (getenv('OPENSHIFT_MYSQL_DB_USERNAME')) ? getenv('OPENSHIFT_MYSQL_DB_USERNAME') : putenv('OPENSHIFT_MYSQL_DB_USERNAME=mysqluser') ;
-$pass = (getenv('OPENSHIFT_MYSQL_DB_PASSWORD')) ? getenv('OPENSHIFT_MYSQL_DB_PASSWORD') : putenv('OPENSHIFT_MYSQL_DB_PASSWORD=mysqluserpassword') ;
+$host = (getenv('OPENSHIFT_MYSQL_DB_HOST')) ? getenv('MYSQL_DATABASE_HOST') : putenv('MYSQL_DATABASE_HOST=localhost') ;
+$port = (getenv('OPENSHIFT_MYSQL_DB_PORT')) ? getenv('MYSQL_DATABASE_PORT') : putenv('MYSQL_DATABASE_HOST=3306');
+$user = (getenv('MYSQL_DATABASE_USER')) ? getenv('MYSQL_DATABASE_USER') : putenv('OPENSHIFT_MYSQL_DB_USERNAME=mysqluser') ;
+$pass = (getenv('MYSQL_DATABASE_PASSWORD')) ? getenv('MYSQL_DATABASE_PASSWORD') : putenv('MYSQL_DATABASE_PASSWORD=mysqluserpassword') ;
 
 define('SS_DATABASE_CLASS', 'MySQLPDODatabase');
-define('SS_DATABASE_SERVER', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('SS_DATABASE_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define("SS_DATABASE_USERNAME", getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('SS_DATABASE_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('SS_DATABASE_NAME', getenv('OPENSHIFT_APP_NAME'));
+define('SS_DATABASE_SERVER', getenv('MYSQL_DATABASE_HOST'));
+define('SS_DATABASE_PORT', getenv('MYSQL_DATABASE_PORT'));
+define("SS_DATABASE_USERNAME", getenv('MYSQL_DATABASE_USER'));
+define('SS_DATABASE_PASSWORD', getenv('MYSQL_DATABASE_NAME'));
+define('SS_DATABASE_NAME', getenv('MYSQL_DATABASE_NAME'));
 
 define('SS_DEFAULT_ADMIN_USERNAME', 'admin');
 define('SS_DEFAULT_ADMIN_PASSWORD', 'admin');
